0c597a0ee9518b62a0e82c4bcfe55691374f0f17
DoomTay <bluejedi@cfl.rr.com>
2015-09-12 16:15:02 -0400

    Initial Game files

---
+++ ZodiacFighter/Assets/Fighter.js
@@ -0,0 +1,67 @@
+﻿#pragma strict
+
+public var health : float;
+public var armor : float;
+public var speed : float;
+public var lightAttackDamage : float;
+public var mediumAttackDamage : float;
+public var heavyAttackDamage : float;
+
+public var jumpPower : float;
+
+public var direction : float;
+@HideInInspector
+public var jumping : boolean = false;
+
+private var isGrounded : boolean = true;
+
+function Start () {
+
+}
+
+function Update () {
+	transform.position.x += direction * speed * 0.25;
+	transform.LookAt(transform.position + Vector3(direction,0,0));
+	if(jumping && isGrounded)
+	{
+		GetComponent(Rigidbody).velocity.y = jumpPower;
+		isGrounded = false;
+	}
+}
+
+function OnCollisionEnter ()
+{
+	isGrounded = true;
+}
+
+function Attack (damage : float, delay : float, push : float)
+{
+	WaitForSeconds(delay);
+	var hit : RaycastHit;
+	if(Physics.Raycast(transform.position,transform.forward, hit, 1))
+	{
+		print("Pow");
+		hit.collider.SendMessage("Damage", damage);
+		hit.collider.GetComponent(Rigidbody).AddForce(transform.forward * push, ForceMode.Impulse);
+	}
+}
+
+function LightAttack()
+{
+	Attack(lightAttackDamage,0.5, 1);
+}
+
+function MediumAttack()
+{
+	Attack(mediumAttackDamage,0.75, 1);
+}
+
+function HeavyAttack()
+{
+	Attack(heavyAttackDamage,1, 1);
+}
+
+function Damage (amount : float)
+{
+	health -= amount;
+}
---
+++ ZodiacFighter/Assets/PlayerController.js
@@ -0,0 +1,15 @@
+﻿#pragma strict
+
+private var fighter : Fighter;
+
+function Start () {
+	fighter = GetComponent(Fighter);
+}
+
+function Update () {
+	fighter.direction = Input.GetAxis("Horizontal");
+	fighter.jumping = Input.GetButtonDown("Jump");
+	if(Input.GetButtonDown("Light Attack")) fighter.LightAttack();
+	if(Input.GetButtonDown("Medium Attack")) fighter.MediumAttack();
+	if(Input.GetButtonDown("Heavy Attack")) fighter.HeavyAttack();
+}
