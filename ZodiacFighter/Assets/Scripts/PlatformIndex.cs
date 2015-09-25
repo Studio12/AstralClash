cc837854a64f14f0a29ff8755ce580efa351113b
Brian Walsh <bwalsh524@knights.ucf.edu>
2015-09-24 21:35:08 -0400

    Gitignore, Platform Index
    Changed git ignore for less conflicts, added initial script for
    pathfinding purposes.

---
+++ ZodiacFighter/Assets/Scripts/PlatformIndex.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlatformIndex : MonoBehaviour {
+
+	public GameObject[] platforms;
+	public float[] distances;
+
+	// Use this for initialization
+	void Start () {
+
+		int i = 0;
+
+		foreach (GameObject p in platforms) {
+		
+			distances[i] = Vector2.Distance(this.transform.position, p.transform.position);
+			i++;
+
+		}
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
