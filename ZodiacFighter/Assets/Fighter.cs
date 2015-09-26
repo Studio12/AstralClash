06a2af97cc08ada1d82e44697e28d21e10f635ab
Brian Walsh <bwalsh524@knights.ucf.edu>
2015-09-25 23:51:24 -0400

    So much
    Lots of stuff. It's a game now?

--- ZodiacFighter/Assets/Fighter.cs
+++ ZodiacFighter/Assets/Fighter.cs
@@ -1,7 +1,8 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Fighter : MonoBehaviour {
+public class Fighter : MonoBehaviour
+{
 
 	[System.Serializable]
 	public class Attack
@@ -13,6 +14,7 @@ public class Fighter : MonoBehaviour {
 		public float reach;
 		public float armor;
 		public float armorBreak;
+		public float knockback;
 	}
 	
 	public float health;
@@ -22,35 +24,33 @@ public class Fighter : MonoBehaviour {
 	public Attack mediumAttack;
 	public Attack heavyAttack;
 	public float cooldown;
-	
 	public float jumpPower;
-	
 	public float direction;
-
 	public float jumping;
-	
 	private bool isGrounded = true;
-
 	public int playID;
 
 	// Use this for initialization
-	void Start () {
+	void Start ()
+	{
 	
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		transform.position += new Vector3(direction * speed * 0.25f,0.0f,0.0f);
-		transform.LookAt(transform.position + new Vector3(0,0,direction));
-		if(jumping>0)
-		{
-			if(isGrounded){
-			GetComponent<Rigidbody2D>().velocity = new Vector3(0,jumpPower,0);
-			isGrounded = false;
+	void Update ()
+	{
+
+		transform.position += new Vector3 (direction * speed * 0.25f, 0.0f, 0.0f);
+		transform.LookAt (transform.position + new Vector3 (0, 0, direction));
+		if (jumping > 0) {
+			if (isGrounded) {
+				GetComponent<Rigidbody2D> ().velocity = new Vector3 (0, jumpPower, 0);
+				isGrounded = false;
 			}
 		}
-		if(cooldown > 0) cooldown -= Time.deltaTime;
-		Debug.DrawLine(transform.position, transform.position + transform.right * 1);
+		if (cooldown > 0)
+			cooldown -= Time.deltaTime;
+		Debug.DrawLine (transform.position, transform.position + transform.right * 1);
 	}
 
 	void OnCollisionEnter2D ()
@@ -58,39 +58,52 @@ public class Fighter : MonoBehaviour {
 		isGrounded = true;
 	}
 	
-	IEnumerator PerformAttack (Attack attack, float push)
+	IEnumerator PerformAttack (Attack attack)
 	{
-		print("Whoosh from " + gameObject.name);
+		float tempSpeed = speed;
+		print ("Whoosh from " + gameObject.name);
 		armor = attack.armor;
-		cooldown = attack.recovery;
+		cooldown = attack.recovery + attack.prep;
 		while (armor > 0) {
+			speed = 0;
 			yield return new WaitForSeconds (attack.prep);
-			RaycastHit2D hit = Physics2D.Raycast (transform.position, transform.right, 5);
+			RaycastHit2D hit = Physics2D.Raycast (transform.position, transform.right, attack.reach);
+			if(hit)Debug.DrawLine(transform.position, hit.transform.position, Color.blue, 3);
 			if (hit.collider != null && hit.collider != gameObject.GetComponent<Collider2D> ()) {
 				print ("Pow from " + gameObject.name);
 				hit.collider.SendMessage ("Damage", attack.damage);
 				hit.collider.SendMessage ("ArmorDamage", attack.armorBreak);
-				hit.collider.GetComponent<Rigidbody2D> ().AddForce (new Vector2 (direction * push, 0), ForceMode2D.Impulse);
+				hit.collider.GetComponent<Rigidbody2D> ().AddForce (new Vector2 (direction * attack.knockback, 0), ForceMode2D.Impulse);
 			}
 			if (attack.projectile)
 				Instantiate (attack.projectile, transform.position, transform.rotation);
 			armor = 0;
+			speed = tempSpeed;
 		}
 	}
 	
-	public void LightAttack()
+	public void LightAttack ()
 	{
-		if(cooldown <= 0) StartCoroutine(PerformAttack(lightAttack, 1));
+		if (cooldown <= 0) {
+			StartCoroutine (PerformAttack (lightAttack));
+			this.GetComponentInChildren<Animator> ().SetTrigger ("Light");
+		}
 	}
 	
-	public void MediumAttack()
+	public void MediumAttack ()
 	{
-		if(cooldown <= 0) StartCoroutine(PerformAttack(mediumAttack, 1));
+		if (cooldown <= 0) {
+			StartCoroutine (PerformAttack (mediumAttack));
+			this.GetComponentInChildren<Animator> ().SetTrigger ("Medium");
+		}
 	}
 	
-	public void HeavyAttack()
+	public void HeavyAttack ()
 	{
-		if(cooldown <= 0) StartCoroutine(PerformAttack(heavyAttack, 1));
+		if (cooldown <= 0) {
+			StartCoroutine (PerformAttack (heavyAttack));
+			this.GetComponentInChildren<Animator> ().SetTrigger ("Heavy");
+		}
 	}
 	
 	void Damage (float amount)
