2814442deee1b181cc7a20c8ead98598c90d5b03
Brian Walsh <bwalsh524@knights.ucf.edu>
2015-10-09 03:06:10 -0400

    Working basic pathfinding and platforming
    as it says

---
+++ ZodiacFighter/Assets/Scripts/JumpHeightTest.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class JumpHeightTest : MonoBehaviour {
+
+
+
+	// Use this for initialization
+	void Start () {
+
+		StartCoroutine ("jump");
+
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+	
+	}
+
+
+}
